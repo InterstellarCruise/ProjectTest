2dc95fa6ce883c3e2d4e2bbf72ceda75007ed5db
InterstellarCruise <1063224@hr.nl>
2023-05-16 11:03:41 +0200

    a

--- ProjectTest/UnitTest1.cs
+++ ProjectTest/UnitTest1.cs
@@ -4,7 +4,35 @@ namespace ProjectTest;
 public class UnitTest1
 {
     [TestMethod]
-    public void TestMethod1()
+    public void CheckAccountEmailTest()
     {
+        AccountsLogic accounts = new AccountsLogic();
+        string validemail = "di@g.com";
+        string invalidemail = "mi@";
+        Assert.IsTrue(accounts.CheckEmail(validemail));
+        Assert.IsFalse(accounts.CheckEmail(invalidemail));
+    }
+    [DataTestMethod]
+    [DataRow(null, null)]
+    [DataRow("user@", null)]
+    [DataRow(null, "password")]
+    [DataRow("di@g.com", "password")]
+    [DataRow("email@", "123")]
+    [DataRow("di@g.com", "123")]
+    public void CheckLoginTest(string email, string password)
+    {
+        AccountsLogic accounts = new AccountsLogic();
+        AccountModel account = accounts.CheckLogin(email, password);
+        bool accountornot = false;
+        if (account == null)
+        {
+            accountornot = true;
+            Assert.IsTrue(accountornot);
+        }
+        else
+        {
+            Assert.IsFalse(accountornot);
+            Assert.AreEqual("Diana Faliuta", account.FullName);
+        }
     }
 }
