50a84a5bfa127386a495d81ff34375aa85af4cc3
AngelovdBroek <1055539@hr.nl>
2023-05-25 15:45:14 +0200

    Merge LastID

--- ProjectTest/UnitTest1.cs
+++ ProjectTest/UnitTest1.cs
@@ -4,41 +4,6 @@ namespace ProjectTest;
 public class UnitTest1
 {
     [TestMethod]
-    public void CheckAccountEmailTest()
-    {
-        // AccountsLogic accounts = new AccountsLogic();
-        // string validemail = "di@g.com";
-        // string invalidemail = "mi@";
-        // Assert.IsTrue(accounts.CheckEmail(validemail));
-        // Assert.IsFalse(accounts.CheckEmail(invalidemail));
-    }
-    // [DataTestMethod]
-    // [DataRow(null, null)]
-    // [DataRow("user@", null)]
-    // [DataRow(null, "password")]
-    // [DataRow("di@g.com", "password")]
-    // [DataRow("email@", "123")]
-    // [DataRow("di@g.com", "123")]
-
-
-    // [TestMethod]
-    // public void CheckLoginTest(string email, string password)
-    // {
-    //     AccountsLogic accounts = new AccountsLogic();
-    //     AccountModel account = accounts.CheckLogin(email, password);
-    //     bool accountornot = false;
-    //     if (account == null)
-    //     {
-    //         accountornot = true;
-    //         Assert.IsTrue(accountornot);
-    //     }
-    //     else
-    //     {
-    //         Assert.IsFalse(accountornot);
-    //         Assert.AreEqual("Diana Faliuta", account.FullName);
-    //     }
-    // }
-    [TestMethod]
     public void CheckLastIDTest()
     {
 
