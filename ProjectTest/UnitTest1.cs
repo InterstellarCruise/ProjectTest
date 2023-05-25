8891db31a3d1c35fc8bfd03020a6c243258e6d3a
Undertaker <124693454+Undertaker2104@users.noreply.github.com>
2023-05-25 15:10:59 +0200

    unittest milan

--- ProjectTest/UnitTest1.cs
+++ ProjectTest/UnitTest1.cs
@@ -1,7 +1,47 @@
 namespace ProjectTest;
 
+
 [TestClass]
 public class UnitTest1
 {
     [TestMethod]
+    public void CheckAccountEmailTest()
+    {
+        AccountsLogic accounts = new AccountsLogic();
+        string validemail = "test@";
+        string validpassword = "test";
+        string fullname = "test test";
+        accounts.NewAcc(validemail, validpassword, fullname);
+        string invalidemail = "testttt@";
+
+        Assert.IsTrue(accounts.CheckEmail(validemail));
+        Assert.IsFalse(accounts.CheckEmail(invalidemail));
+        accounts.RemoveAcc(validemail);
+    }
+    [TestMethod]
+    public void CheckLoginTest()
+    {
+        AccountsLogic accounts = new AccountsLogic();
+        string email = "test@";
+        string password = "test";
+        string fullname = "test test";
+        accounts.NewAcc(email, password, fullname);
+        AccountModel account = accounts.CheckLogin(email, password);
+
+        Assert.AreEqual("test test", account.FullName);
+        accounts.RemoveAcc(email);
+    }
+    [TestMethod]
+    public void NewAccTest()
+    {
+        AccountModel account = new AccountModel(100000, "test@", "test", "test test");
+        AccountsLogic accounts = new AccountsLogic();
+        accounts.UpdateList(account);
+        AccountModel accountfound = accounts.GetById(100000);
+
+        Assert.AreEqual(accountfound.FullName, account.FullName);
+        accounts.RemoveAcc(account.EmailAddress);
+    }
+
 }
+
