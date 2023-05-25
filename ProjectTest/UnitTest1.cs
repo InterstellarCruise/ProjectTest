11eaec3fefc988f5b86a5e9f321a0ad2bd93f31d
AngelovdBroek <1055539@hr.nl>
2023-05-25 15:47:45 +0200

    Merged LastID

--- ProjectTest/UnitTest1.cs
+++ ProjectTest/UnitTest1.cs
@@ -52,5 +52,48 @@ public class UnitTest1
         AccountModel accountfound = accounts.GetById(100000);
         Assert.IsNull(accountfound);
     }
+    [TestMethod]
+    public void CheckLastIDTest()
+    {
+
+        int id = 99999;
+        List<string> genres = new List<string>();
+        genres.Add("Horror");
+        FilmModel film = new FilmModel(id, "Unit", "test", 0, 2.5, genres);
+        FilmsLogic filmlogic = new FilmsLogic();
+        filmlogic.UpdateList(film);
+        List<FilmModel> _films;
+        _films = FilmsAccess.LoadAll();
+        int last = _films[_films.Count - 1].Id;
+        Assert.AreEqual(id, last);
+    }
+    public void CheckAllFilmsTest()
+    {
+
+    }
+    public void CheckReservationsByAccountTest()
+    {
+
+    }
+    public void CheckDeleteReservationTest()
+    {
+
+    }
+    public void CheckMoviesByDateTest()
+    {
+
+    }
+    public void CheckChooseShowTest()
+    {
+
+    }
+    public void CheckShowsByDateTest()
+    {
+
+    }
+    public void CheckValidShowTimeTest()
+    {
+
+    }
 }
 
