e96e295b130933cc1e13d06b8aca74033b8d16c5
AngelovdBroek <1055539@hr.nl>
2023-06-01 14:26:47 +0200

    Valid show time, valid show date done

--- ProjectTest/UnitTest1.cs
+++ ProjectTest/UnitTest1.cs
@@ -1,12 +1,60 @@
 namespace ProjectTest;
 
+
 [TestClass]
 public class UnitTest1
 {
     [TestMethod]
+    public void CheckAccountEmailTest()
+    {
+        AccountsLogic accounts = new AccountsLogic();
+        string validemail = "test@";
+        string validpassword = "test";
+        string fullname = "test test";
+        accounts.NewAcc(validemail, validpassword, fullname);
+        string invalidemail = "testttt@";
+
+        Assert.IsTrue(accounts.CheckEmail(validemail));
+        Assert.IsFalse(accounts.CheckEmail(invalidemail));
+        accounts.RemoveAcc(validemail);
+    }
+    [TestMethod]
+    public void CheckLoginTest()
+    {
+        AccountsLogic accounts = new AccountsLogic();
+        string email = "test@";
+        string password = "test";
+        string fullname = "test test";
+        accounts.NewAcc(email, password, fullname);
+        AccountModel account = accounts.CheckLogin(email, password);
+
+        Assert.AreEqual("test test", account.FullName);
+        accounts.RemoveAcc(email);
+    }
+    [TestMethod]
+    public void NewAccTest()
+    {
+        AccountModel account = new AccountModel(100000, "test@", "test", "test test");
+        AccountsLogic accounts = new AccountsLogic();
+        accounts.UpdateList(account);
+        AccountModel accountfound = accounts.GetById(100000);
+
+        Assert.AreEqual(accountfound.FullName, account.FullName);
+        accounts.RemoveAcc(account.EmailAddress);
+    }
+    [TestMethod]
+    public void RemoveAcc()
+    {
+        AccountModel account = new AccountModel(100000, "test@", "test", "test test");
+        AccountsLogic accounts = new AccountsLogic();
+        accounts.UpdateList(account);
+        accounts.RemoveAcc(account.EmailAddress);
+        AccountModel accountfound = accounts.GetById(100000);
+        Assert.IsNull(accountfound);
+    }
+    [TestMethod]
     public void CheckLastIDTest()
     {
-
         int id = 99999;
         List<string> genres = new List<string>();
         genres.Add("Horror");
@@ -32,7 +80,6 @@ public class UnitTest1
     }
     public void CheckMoviesByDateTest()
     {
-
     }
     public void CheckChooseShowTest()
     {
@@ -40,10 +87,13 @@ public class UnitTest1
     }
     public void CheckShowsByDateTest()
     {
-
+        ShowsLogic showlogic = new ShowsLogic();
+        Assert.IsFalse(showlogic.ValidShowDate("2023-13-13"));
     }
+    [TestMethod]
     public void CheckValidShowTimeTest()
     {
-
+        ShowsLogic showlogic = new ShowsLogic();
+        Assert.IsFalse(showlogic.ValidShowTime("25.30"));
     }
 }
